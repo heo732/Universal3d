818987664fcab0174c3ea621b5f73822ecb28345
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-04-11 22:52:15 +0300

    Implemented BlockWriter.cs

--- source/U3DSpace/U3DSpace/IO/BlockIO/BlockWriter.cs
+++ source/U3DSpace/U3DSpace/IO/BlockIO/BlockWriter.cs
@@ -1,6 +1,216 @@
-﻿namespace U3DSpace.IO.BlockIO
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace U3DSpace.IO.BlockIO
 {
+    /// <summary>
+    /// Represents byte writer for the block.
+    /// Supports only uncompressed values.
+    /// </summary>
     public class BlockWriter
     {
+        #region Fields
+
+        private Block _block;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public BlockWriter()
+        {
+            _block = new Block();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public Block GetBlock(uint blockType)
+        {
+            AlignTo4Byte();
+            return new Block()
+            {
+                BlockType = blockType,
+                Data = new List<byte>(_block.Data),
+                MetaData = new List<byte>(_block.MetaData),
+                DataAligning = new List<byte>(_block.DataAligning),
+                MetaDataAligning = new List<byte>(_block.MetaDataAligning)
+            };
+        }
+
+        public void WriteArray(IEnumerable<float> array)
+        {
+            foreach (float item in array)
+            {
+                WriteF32(item);
+            }
+        }
+
+        public void WriteBlock(Block block)
+        {
+            WriteU32(block.BlockType);
+            WriteU32((uint)block.Data.Count);
+            WriteU32((uint)block.MetaData.Count);
+            WriteData(block.Data);
+            WriteData(block.DataAligning);
+            WriteData(block.MetaData);
+            WriteData(block.MetaDataAligning);
+        }
+
+        public void WriteData(IEnumerable<byte> data)
+        {
+            foreach (byte b in data)
+            {
+                WriteU8(b);
+            }
+        }
+
+        public void WriteF32(float value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteF64(double value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteI16(short value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteI32(int value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaData(IEnumerable<byte> metaData)
+        {
+            foreach (byte b in metaData)
+            {
+                _block.MetaData.Add(b);
+            }
+        }
+
+        public void WriteMetaF32(float value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaF64(double value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaI16(short value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaI32(int value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaString(string s, Encoding encoding)
+        {
+            WriteMetaU16((ushort)s.Length);
+            WriteMetaData(encoding.GetBytes(s));
+        }
+
+        public void WriteMetaU16(ushort value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaU32(uint value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaU64(ulong value)
+        {
+            WriteMetaData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteMetaU8(byte value)
+        {
+            _block.MetaData.Add(value);
+        }
+
+        public void WritePadding()
+        {
+            byte paddingSize = (byte)(4 - (_block.Data.Count % 4));
+            if (paddingSize != 4)
+            {
+                for (byte i = 0; i < paddingSize; i++)
+                {
+                    WriteU8(0);
+                }
+            }
+        }
+
+        public void WriteString(string s, Encoding encoding)
+        {
+            WriteU16((ushort)s.Length);
+            WriteData(encoding.GetBytes(s));
+        }
+
+        public void WriteU16(ushort value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteU32(uint value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteU64(ulong value)
+        {
+            WriteData(BitConverter.GetBytes(value));
+        }
+
+        public void WriteU8(byte value)
+        {
+            _block.Data.Add(value);
+        }
+
+        private void AlignData()
+        {
+            byte aligningSize = (byte)(4 - (_block.Data.Count % 4));
+            _block.DataAligning = new List<byte>();
+            if (aligningSize != 4)
+            {
+                for (byte i = 0; i < aligningSize; i++)
+                {
+                    _block.DataAligning.Add(0);
+                }
+            }
+        }
+
+        private void AlignMetaData()
+        {
+            byte aligningSize = (byte)(4 - (_block.MetaData.Count % 4));
+            _block.MetaDataAligning = new List<byte>();
+            if (aligningSize != 4)
+            {
+                for (byte i = 0; i < aligningSize; i++)
+                {
+                    _block.MetaDataAligning.Add(0);
+                }
+            }
+        }
+
+        private void AlignTo4Byte()
+        {
+            AlignData();
+            AlignMetaData();
+        }
+
+        #endregion Methods
     }
 }
