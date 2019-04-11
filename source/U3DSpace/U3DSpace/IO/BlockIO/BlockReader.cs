ca89dcf8e74285a58d6d1970985aa7ef7b9ecc55
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-04-12 00:25:49 +0300

    Implemented ReadMeta methods

--- source/U3DSpace/U3DSpace/IO/BlockIO/BlockReader.cs
+++ source/U3DSpace/U3DSpace/IO/BlockIO/BlockReader.cs
@@ -122,6 +122,157 @@ namespace U3DSpace.IO.BlockIO
             }
         }
 
+        public bool TryToReadMetaF32(out float value)
+        {
+            if (MetaDataSise < 4)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToSingle(_block.MetaData.GetRange(0, 4).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 4);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaF64(out double value)
+        {
+            if (MetaDataSise < 8)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToDouble(_block.MetaData.GetRange(0, 8).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 8);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaI16(out short value)
+        {
+            if (MetaDataSise < 2)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToInt16(_block.MetaData.GetRange(0, 2).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 2);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaI32(out int value)
+        {
+            if (MetaDataSise < 4)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToInt32(_block.MetaData.GetRange(0, 4).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 4);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaString(Encoding encoding, out string value)
+        {
+            if (TryToReadMetaU16(out ushort length))
+            {
+                if (length > 0)
+                {
+                    if (MetaDataSise >= length)
+                    {
+                        value = encoding.GetString(_block.MetaData.GetRange(0, length).ToArray());
+                        _block.MetaData.RemoveRange(0, length);
+                        return true;
+                    }
+                    else
+                    {
+                        value = string.Empty;
+                        return false;
+                    }
+                }
+                else
+                {
+                    value = string.Empty;
+                    return true;
+                }
+            }
+            else
+            {
+                value = string.Empty;
+                return false;
+            }
+        }
+
+        public bool TryToReadMetaU16(out ushort value)
+        {
+            if (MetaDataSise < 2)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToUInt16(_block.MetaData.GetRange(0, 2).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 2);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaU32(out uint value)
+        {
+            if (MetaDataSise < 4)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToUInt32(_block.MetaData.GetRange(0, 4).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 4);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaU64(out ulong value)
+        {
+            if (MetaDataSise < 8)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = BitConverter.ToUInt64(_block.MetaData.GetRange(0, 8).ToArray(), 0);
+                _block.MetaData.RemoveRange(0, 8);
+                return true;
+            }
+        }
+
+        public bool TryToReadMetaU8(out byte value)
+        {
+            if (IsMetaDataEmpty)
+            {
+                value = 0;
+                return false;
+            }
+            else
+            {
+                value = _block.MetaData[0];
+                _block.MetaData.RemoveAt(0);
+                return true;
+            }
+        }
+
         public bool TryToReadString(Encoding encoding, out string value)
         {
             if (TryToReadU16(out ushort length))
