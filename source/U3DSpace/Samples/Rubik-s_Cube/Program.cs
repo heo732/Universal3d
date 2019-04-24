02358cbd0f068c0d6aa619bcc3692d5a9a0c7acb
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-04-24 19:57:58 +0300

    Added empty sample 'Rubik-s_Cube'

---
+++ source/U3DSpace/Samples/Rubik-s_Cube/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rubik_s_Cube
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        #endregion Methods
+    }
+}
